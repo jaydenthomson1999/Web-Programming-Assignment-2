35e66e93d3598917b2f042329a6b3108318c91f9
Jayden Thomson <jaydenthomson1999@gmail.com>
2019-09-21 22:52:52 +1000

    add mongo refactor for add-group

--- server/api/add-group.js
+++ server/api/add-group.js
@@ -5,78 +5,141 @@
 */
 
 module.exports = function(app, fs) {
+    const dbSettings = require('../db-settings');
+
     app.put('/api/add-group', (req, res) => {
-        let groupName = req.body.groupName;
-        let username = req.body.username;
-        let foundUser = false;
-        let userIndex;
+        // let groupName = req.body.groupName;
+        // let username = req.body.username;
+        // let foundUser = false;
+        // let userIndex;
 
-        console.log(req.body);
+        // console.log(req.body);
     
-        fs.readFile(__dirname + '/../users.json', (err, data) => {
-            if(err) {
-                console.log(err);
-                res.json({'err': err});
-            } else {
-                //find user in users json list
-                let users = JSON.parse(data);
+        // fs.readFile(__dirname + '/../users.json', (err, data) => {
+        //     if(err) {
+        //         console.log(err);
+        //         res.json({'err': err});
+        //     } else {
+        //         //find user in users json list
+        //         let users = JSON.parse(data);
 
-                for(user in users.users) {
-                    if(users.users[user].username === username) {
-                        foundUser = true;
-                        userIndex = user;
-                        break;
-                    }
-                }
+        //         for(user in users.users) {
+        //             if(users.users[user].username === username) {
+        //                 foundUser = true;
+        //                 userIndex = user;
+        //                 break;
+        //             }
+        //         }
 
-                if(foundUser){
-                    // check if they are super or groupAdmin
-                    if( users.users[userIndex].type === 'super' || 
-                    users.users[userIndex].type === 'group admin') {
-                        // check if groupName already exists
-                        let groupExits = false;
+        //         if(foundUser){
+        //             // check if they are super or groupAdmin
+        //             if( users.users[userIndex].type === 'super' || 
+        //             users.users[userIndex].type === 'group admin') {
+        //                 // check if groupName already exists
+        //                 let groupExits = false;
 
-                        for(group in users.users[userIndex].adminGroupList) {
-                            if(users.users[userIndex].adminGroupList[group]
-                                === groupName) {
-                                groupExits = true;
-                            }
-                        }
+        //                 for(group in users.users[userIndex].adminGroupList) {
+        //                     if(users.users[userIndex].adminGroupList[group]
+        //                         === groupName) {
+        //                         groupExits = true;
+        //                     }
+        //                 }
 
-                        if(groupExits){
-                            res.json({'add': false, 'comment': 'group exsits'});
-                            return;
-                        }
+        //                 if(groupExits){
+        //                     res.json({'add': false, 'comment': 'group exsits'});
+        //                     return;
+        //                 }
 
-                        // add groupName to user
-                        let groupJson = {
-                            'groupName': groupName, 
-                            'channels': ['main']
-                        };
-                        users.users[userIndex].adminGroupList.push(groupName);
-                        users.users[userIndex].groupList.push(groupJson);
+        //                 // add groupName to user
+        //                 let groupJson = {
+        //                     'groupName': groupName, 
+        //                     'channels': ['main']
+        //                 };
+        //                 users.users[userIndex].adminGroupList.push(groupName);
+        //                 users.users[userIndex].groupList.push(groupJson);
                         
-                        fs.writeFile(__dirname + '/../users.json', 
-                        JSON.stringify(users) , err => {
-                            if(err) {
-                                console.log(err);
-                                res.json({'err': err});
-                                return;
-                            } else{
-                                res.json({'add': true});
-                                return;
-                            }
-                        });
-                    }
-                    else {
-                        // user does not have priviledge
-                        res.json({'add': false, 'comment': 'no priviledge'});
-                        return;
-                    }                    
-                } else {
-                    res.json({'add': false})
+        //                 fs.writeFile(__dirname + '/../users.json', 
+        //                 JSON.stringify(users) , err => {
+        //                     if(err) {
+        //                         console.log(err);
+        //                         res.json({'err': err});
+        //                         return;
+        //                     } else{
+        //                         res.json({'add': true});
+        //                         return;
+        //                     }
+        //                 });
+        //             }
+        //             else {
+        //                 // user does not have priviledge
+        //                 res.json({'add': false, 'comment': 'no priviledge'});
+        //                 return;
+        //             }                    
+        //         } else {
+        //             res.json({'add': false})
+        //         }
+        //     }
+        // });
+        dbSettings.MongoClient.connect(dbSettings.url, 
+        {poolSize:10,useNewUrlParser: true, useUnifiedTopology: true},
+        function(err, client) {
+            if(err) throw new Error(err);
+
+            const db = client.db(dbSettings.dbName);
+            const collection = db.collection('users');
+            const group = req.body.groupName;
+            const userid = req.body.userid;
+            let objectid = new dbSettings.ObjectID(userid);
+            
+            collection.find({_id: objectid}).limit(1).toArray((err, data) => {
+                if(err) return res.send(err);
+
+                if(data.length == 0) {
+                    return res.send({
+                        'add': false, 
+                        'comment': 'user doesnt exist'
+                    });
                 }
-            }
+
+                // user has permission
+                if(data[0].type === 'super' || data[0].type === 'group admin') {
+                    collection.find({
+                        groupList: {$elemMatch: {groupName: group}}
+                    }).count((err, count) => {
+                        if(err) return res.send(err);
+
+                        if(count === 0) {
+                            let groupItem = {
+                                groupName: group,
+                                channels: [
+                                    "main"
+                                ]
+                            };
+
+                            collection.updateOne(
+                                {_id: objectid},
+                                {$push: 
+                                    {groupList: groupItem, 
+                                    adminGroupList: group }
+                                },
+                                () => {
+                                    return res.send({'add': true});
+                                }
+                            );
+                        } else {
+                            return res.send({
+                                'add': false, 
+                                'comment': 'group exists'
+                            });
+                        }
+                    });
+                } else {
+                    return res.send({
+                        'add': false, 
+                        'comment': 'no permission'
+                    });
+                }
+            });
         });
     });
 }
--- server/db-settings.js
+++ server/db-settings.js
@@ -10,33 +10,33 @@ module.exports.mongodb = mongodb;
 module.exports.ObjectID = ObjectId;
 module.exports.dbName = dbName;
 
-// MongoClient.connect(url, {poolSize:10,useNewUrlParser: true,
-//     useUnifiedTopology: true},function(err, client) {
-//         const dbName = 'chat-room';
-//         const db = client.db(dbName);
-//         let user = {"username":"super","email":"admin@chat.com","password":"super","type":"super","groupList":[{"groupName":"test group","channels":["main","test channel"]},{"groupName":"one more test","channels":["main"]}],"adminGroupList":["test group","one more test"]}
-//         const collection = db.collection('users');
+MongoClient.connect(url, {poolSize:10,useNewUrlParser: true,
+    useUnifiedTopology: true},function(err, client) {
+        const dbName = 'chat-room';
+        const db = client.db(dbName);
+        let user = {"username":"super","email":"admin@chat.com","password":"super","type":"super","groupList":[{"groupName":"test group","channels":["main","test channel"]},{"groupName":"one more test","channels":["main"]}],"adminGroupList":["test group","one more test"]}
+        const collection = db.collection('users');
 
-//         collection.find({'username': user.username}).count((err, count) => {
-//             if(count == 0) {
-//                 collection.insertOne(user, (err, dbres) => {
-//                     if(err) throw err;
-//                     console.log("Initizalised Database with Super");
-//                 });
-//             } else {
-//                 console.log("Super is ready");
-//             }
-//         });
-// });
+        collection.find({'username': user.username}).count((err, count) => {
+            if(count == 0) {
+                collection.insertOne(user, (err, dbres) => {
+                    if(err) throw err;
+                    console.log("Initizalised Database with Super");
+                });
+            } else {
+                console.log("Super is ready");
+            }
+        });
+});
 
-// MongoClient.connect(url, {poolSize:10,useNewUrlParser: true,
-//     useUnifiedTopology: true},function(err, client) {
-//         const dbName = 'chat-room';
-//         const db = client.db(dbName);
-//         let user = {"username":"super","email":"admin@chat.com","password":"super","type":"super","groupList":[{"groupName":"test group","channels":["main","test channel"]},{"groupName":"one more test","channels":["main"]}],"adminGroupList":["test group","one more test"]}
-//         const collection = db.collection('users');
+MongoClient.connect(url, {poolSize:10,useNewUrlParser: true,
+    useUnifiedTopology: true},function(err, client) {
+        const dbName = 'chat-room';
+        const db = client.db(dbName);
+        let user = {"username":"super","email":"admin@chat.com","password":"super","type":"super","groupList":[{"groupName":"test group","channels":["main","test channel"]},{"groupName":"one more test","channels":["main"]}],"adminGroupList":["test group","one more test"]}
+        const collection = db.collection('users');
 
-//         collection.find({'username': user.username}).limit(1).toArray((err, data) => {
-//             console.log(data);
-//         });
-// });
+        collection.find({'username': user.username}).limit(1).toArray((err, data) => {
+            console.log(data);
+        });
+});
