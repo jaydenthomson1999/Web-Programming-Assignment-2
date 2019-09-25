d714ef014d84774796474cb3897a21de6b77294c
Jayden Thomson <jaydenthomson1999@gmail.com>
2019-09-25 10:43:08 +1000

    finished front end port for mongodb backend

--- src/app/group-list/group-list.component.ts
+++ src/app/group-list/group-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { UserService } from '../user/user.service';
 
 interface Delete {
   delete: boolean;
@@ -51,8 +52,8 @@ export class GroupListComponent implements OnInit {
   private type;
   private modalList = [];
 
-  constructor(private http: HttpClient) {
-    this.user = JSON.parse(sessionStorage.getItem('user')).username;
+  constructor(private http: HttpClient, private userService: UserService) {
+    this.user = JSON.parse(sessionStorage.getItem('user'));
     this.getGroups();
   }
 
@@ -72,61 +73,40 @@ export class GroupListComponent implements OnInit {
 
   // performs get groups request
   getGroups() {
-    const data = new Promise<any>((resolve, reject) => {
-      this.http.post<GetGroups>(this.getGroupUrl, {
-        username: (JSON.parse(sessionStorage.getItem('user')).username)
-      }).subscribe(
-        res => {
-          if (res.ok) {
-            resolve(res);
-          } else {
-            resolve(false);
-          }
-        },
-        (err: HttpErrorResponse) => {
-          console.log(err.error);
-          reject(err.error);
+    this.userService.getGroup(JSON.parse(sessionStorage.getItem('user'))._id)
+    .subscribe(
+      res => {
+        if (res.ok) {
+          this.adminGroupList = res.adminGroupList;
+          this.groupList = res.groupList;
+        } else {
+          alert(res.comment);
         }
-      );
-    });
-
-    data.then(json => {
-      this.adminGroupList = json.adminGroupList;
-      this.groupList = json.groupList;
-    });
+      },
+      (err: HttpErrorResponse) => {
+        alert(err.error);
+      }
+    );
   }
 
   // performs delete groups request
   deleteGroup(groupName: string) {
-    // delete group
-    const httpOptions = {
-      headers: new HttpHeaders({'Content-Type': 'application/json'}),
-      body: {username: this.user.username, groupName}
-    };
-
-    const del = new Promise((resolve, reject) => {
-      this.http.delete<Delete>(this.delUrl, httpOptions).subscribe(
-        res => {
-          if (res.delete) {
-            resolve(res.delete);
-          } else {
-            resolve(false);
-          }
-        },
-        (err: HttpErrorResponse) => {
-          console.log(err.error);
-          reject(err.error);
+    this.userService.delGroup(
+      JSON.parse(sessionStorage.getItem('user'))._id,
+      groupName
+    )
+    .subscribe(
+      res => {
+        if (res.delete) {
+          this.getGroups();
+        } else {
+          alert(res.comment);
         }
-      );
-    });
-
-    del.then(bool => {
-      if (bool) {
-        this.getGroups();
-      } else {
-        alert('Couldn\'t delete user');
+      },
+      (err: HttpErrorResponse) => {
+        alert(err.error);
       }
-    });
+    );
   }
 
   // presents modal
@@ -135,51 +115,47 @@ export class GroupListComponent implements OnInit {
     this.operation = operation;
     this.type = type;
 
-    const users = new Promise<any>((resolve, reject) => {
-      this.http.get<GetUsers>(this.getUsersUrl).subscribe(
-        res => {
-          if (res.ok) {
-            resolve(res.users.users);
-          } else {
-            resolve(false);
-          }
-        },
-        (err: HttpErrorResponse) => {
-          console.log(err.error);
-          reject(err.error);
-        }
-      );
-    });
+    this.userService.getUser().subscribe(
+      res => {
+        const id = JSON.parse(sessionStorage.getItem('user'))._id;
+        const adminIndex = res.findIndex((user) => user._id === id);
+        res.splice(adminIndex, 1);
 
-    users.then(data => {
-      if (data) {
-        data.forEach(user => {
-          let isaMember = false;
-
-          for (const group of user.groupList) {
-            if (type === 'group' && group.groupName === name) {
-              this.selectedGroup = name;
-              isaMember = true;
-              break;
-            } else if (type === 'channel'
-              && group.groupName === this.selectedGroup) {
-                for (const channel of group.channels) {
-                  if (channel === name) {
-                    this.selectedChannel = name;
-                    isaMember = true;
+        if (res.length) {
+          res.forEach(user => {
+            let isaMember = false;
+            for (const group of user.groupList) {
+              if (type === 'group' && group.groupName === name) {
+                this.selectedGroup = name;
+                isaMember = true;
+                break;
+              } else if (type === 'channel'
+                && group.groupName === this.selectedGroup) {
+                  for (const channel of group.channels) {
+                    if (channel === name) {
+                      this.selectedChannel = name;
+                      isaMember = true;
+                    }
                   }
-                }
+              }
             }
-          }
+            if ((!isaMember && operation === 'Add') ||
+                (isaMember && operation === 'Delete')) {
+                this.modalList.push(user);
+            }
+          });
 
-          if ((!isaMember && operation === 'Add') ||
-              (isaMember && operation === 'Delete')) {
-              this.modalList.push(user.username);
+          if (!this.modalList.length) {
+            alert('No Users');
           }
-
-        });
+        } else {
+          alert('No Users');
+        }
+      },
+      (err: HttpErrorResponse) => {
+        alert(err.error);
       }
-    });
+    );
   }
 
   // changes the selected user
@@ -189,128 +165,99 @@ export class GroupListComponent implements OnInit {
 
   // trigers modal operation
   modalConfirm() {
-    let bool: Promise<any>;
-
     if (this.operation === 'Add') {
       if (this.type === 'group') {
-        bool = this.addUserGroup(this.user, this.selectedUser,
-          this.selectedGroup);
+        this.addUserGroup(this.user, this.selectedUser, this.selectedGroup);
 
       } else if (this.type === 'channel') {
-        bool = this.addUserChannel(this.user, this.selectedUser,
+        this.addUserChannel(this.user, this.selectedUser,
           this.selectedGroup, this.selectedChannel);
 
       }
     } else if (this.operation === 'Delete') {
       if (this.type === 'group') {
-        bool = this.delUserGroup(this.user, this.selectedUser,
+        this.delUserGroup(this.user, this.selectedUser,
           this.selectedGroup);
 
       } else if (this.type === 'channel') {
-        bool = this.delUserChannel(this.user, this.selectedUser,
+        this.delUserChannel(this.user, this.selectedUser,
           this.selectedGroup, this.selectedChannel);
       }
     }
-
-    bool.then(ok => {
-      if (ok) {
-        this.getGroups();
-        this.channelList = [];
-      } else {
-        alert('Could not perform modal operation');
-      }
-    });
-
   }
 
   // adds user to group in backend
   private addUserGroup(adminUser, addUser, groupName) {
-    return new Promise((resolve, reject) => {
-      this.http.put<Put>(this.addUserGroupUrl,
-        { adminUser, addUser, groupName }).subscribe(
-          res => {
-            if (res.add) {
-              resolve(res.add);
-            } else {
-              resolve(false);
-            }
-          },
-          (err: HttpErrorResponse) => {
-            console.log(err.error);
-            reject(err.error);
-          }
-      );
-    });
+    this.userService.addUserToGroup(adminUser._id, addUser._id, groupName)
+    .subscribe(
+      res => {
+        if (res.add) {
+          this.getGroups();
+          this.channelList = [];
+        } else {
+          alert(res.comment);
+        }
+      },
+      (err: HttpErrorResponse) => {
+        alert(err);
+      }
+    );
   }
 
   // adds user to channel in back end
   private addUserChannel(adminUser, addUser, groupName, channelName) {
-    return new Promise((resolve, reject) => {
-      this.http.put<Put>(this.addUserChannelUrl,
-        { adminUser, addUser, groupName, channelName }).subscribe(
-          res => {
-            console.log(res);
-            if (res.add) {
-              resolve(res.add);
-            } else {
-              resolve(false);
-            }
-          },
-          (err: HttpErrorResponse) => {
-            console.log(err.error);
-            reject(err.error);
-          }
-        );
-    });
+    this.userService.addUserToChannel(adminUser._id, addUser._id, groupName,
+                                      channelName)
+    .subscribe(
+      res => {
+        if (res.add) {
+          this.getGroups();
+          this.channelList = [];
+        } else {
+          alert(res.comment);
+        }
+      },
+      (err: HttpErrorResponse) => {
+        alert(err);
+      }
+    );
   }
 
   // deletes user from a group in backend
   private delUserGroup(adminUser, delUser, groupName) {
-    const httpOptions = {
-      headers: new HttpHeaders({'Content-Type': 'application/json'}),
-      body: { adminUser, delUser, groupName }
-    };
-
-    return new Promise((resolve, reject) => {
-      this.http.delete<Delete>(this.delUserGroupUrl, httpOptions).subscribe(
-        res => {
-          console.log(res);
-          if (res.delete) {
-            resolve(res.delete);
-          } else {
-            resolve(false);
-          }
-        },
-        (err: HttpErrorResponse) => {
-          console.log(err.error);
-          reject(err.error);
+    this.userService.delUserFromGroup(adminUser._id, delUser._id, groupName)
+    .subscribe(
+      res => {
+        if (res.delete) {
+          this.getGroups();
+          this.channelList = [];
+        } else {
+          alert(res.comment);
         }
-      );
-    });
+      },
+      (err: HttpErrorResponse) => {
+        alert(err);
+      }
+    );
   }
 
   // deletes user from channel in backend
   private delUserChannel(adminUser, delUser, groupName, channelName) {
-    const httpOptions = {
-      headers: new HttpHeaders({'Content-Type': 'application/json'}),
-      body: { adminUser, delUser, groupName, channelName }
-    };
-
-    return new Promise((resolve, reject) => {
-      this.http.delete<Delete>(this.delUserChannelUrl, httpOptions).subscribe(
-        res => {
-          if (res.delete) {
-            resolve(res.delete);
-          } else {
-            resolve(false);
-          }
-        },
-        (err: HttpErrorResponse) => {
-          console.log(err.error);
-          reject(err.error);
+    this.userService.delUserFromChannel(adminUser._id, delUser._id, groupName,
+                                      channelName)
+    .subscribe(
+      res => {
+        if (res.delete) {
+          this.getGroups();
+          this.channelList = [];
+        } else {
+          alert(res.comment);
         }
-      );
-    });
+      },
+      (err: HttpErrorResponse) => {
+        alert(err);
+      }
+    );
   }
 
 }
