e1f0676ffd7966b146bc8de8dc5145418725f807
Jayden Thomson <jaydenthomson1999@gmail.com>
2019-09-26 14:48:01 +1000

    add group-list jasmine test

--- src/app/group-list/group-list.component.spec.ts
+++ src/app/group-list/group-list.component.spec.ts
@@ -3,11 +3,29 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { GroupListComponent } from './group-list.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
+const superUser = {
+  user: {
+    _id: '5d8bff09ab26f839808fdb8d',
+    username: 'super',
+    email: 'admin@chat.com',
+    password: 'super',
+    type: 'super'
+  }
+};
+
+const fakeStorage = {
+  getItem(key) {
+      return JSON.stringify(superUser[key]);
+  }
+};
+
 describe('GroupListComponent', () => {
   let component: GroupListComponent;
   let fixture: ComponentFixture<GroupListComponent>;
 
   beforeEach(async(() => {
+    spyOn(window.sessionStorage, 'getItem').and.callFake(fakeStorage.getItem);
+
     TestBed.configureTestingModule({
       imports: [RouterTestingModule, HttpClientTestingModule],
       declarations: [ GroupListComponent ]
@@ -21,7 +39,13 @@ describe('GroupListComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should not send request to server for unknown operation', () => {
+    spyOn(window, 'alert');
+    component.modalConfirm();
+    expect(window.alert).toHaveBeenCalledWith('Unrecognised operation');
+  });
 });
